908146f3bda6a8604d3defca45edbe53c880b99c
Zheyuan -Robert XU <u6964086@anu.edu.au>
2022-01-07 00:52:25 +1100

    add order in drawer

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -35,7 +35,7 @@ class CartScreen extends StatelessWidget {
                   Spacer(), //takes up all the space
                   Chip(
                     label: Text(
-                      '\$${cart.totalAmount}',
+                      '\$${cart.totalAmount.toStringAsFixed(2)}',
                       style: TextStyle(
                           color: Theme.of(context)
                               .primaryTextTheme
